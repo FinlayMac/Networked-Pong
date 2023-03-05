ab11b51ee8ab00d30035b7cf8248531615978e93
FinlayMac <34044928+FinlayMac@users.noreply.github.com>
2023-03-05 16:06:50 +0000

    Added Chat Rooms

--- Assets/NakamaConnection.cs
+++ Assets/NakamaConnection.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Nakama;
+using Nakama.TinyJson;
 using UnityEngine.Events;
 using UnityEngine.UI;
 using TMPro;
@@ -9,27 +10,51 @@ using System;
 
 public class NakamaConnection : MonoBehaviour
 {
+    //UI Swaps
+    public GameObject LoginPanel;
+    public GameObject ChatRoomPanel;
+    public GameObject MessagePanel;
+
+
+    private const string RoomName = "heroes";
+
     //make sure you use a key for actual deployment
     private readonly IClient client = new Client("http", "127.0.0.1", 7350, "defaultkey");
     private ISession session;
     private ISocket socket;
+    private IChannel channel;
 
     //UI
-    public Button SubmitButton;
-    public TextMeshProUGUI emailInput;
-    public TextMeshProUGUI passwordInput;
+    public Button LoginButton;
+    public Button JoinRoomButton;
+    public Button SendMessageButton;
+    public TMP_InputField emailInput;
+    public TMP_InputField passwordInput;
     public TextMeshProUGUI ErrorLabel;
+    public TMP_InputField MessageInput;
 
-    private void Awake() { SubmitButton.onClick.AddListener(SubmitLogin); }
+    private void Awake()
+    {
+        LoginButton.onClick.AddListener(SubmitLogin);
+        JoinRoomButton.onClick.AddListener(JoinChatRoom);
+        SendMessageButton.onClick.AddListener(SendMessage);
+    }
 
+    public bool Testing = true;
     async void SubmitLogin()
     {
-
-        string email = "MO02FM@uhi.ac.uk";
-        string password = "password";
-
-        // string email = emailInput.text;
-        // string password = passwordInput.text;
+        string email;
+        string password;
+        if (Testing)
+        {
+            email = "";
+            password = "";
+        }
+        else
+        {
+            email = emailInput.text;
+            password = passwordInput.text;
+        }
 
         try
         {
@@ -43,10 +68,52 @@ public class NakamaConnection : MonoBehaviour
         }
         Debug.Log("Started session " + session);
 
+        //creates a new socket and events
         socket = client.NewSocket();
         socket.Connected += () => Debug.Log("Socket Connected");
         socket.Closed += () => Debug.Log("Socket Closed");
 
+        socket.ReceivedChannelMessage += message => Debug.Log("message received " + message);
+
+        //actually connects to the socket
+        await socket.ConnectAsync(session);
+
+        //swaps the UI
+        LoginPanel.SetActive(false);
+        ChatRoomPanel.SetActive(true);
     }
 
+    async void JoinChatRoom()
+    {
+        //swaps the UI
+        ChatRoomPanel.SetActive(false);
+        MessagePanel.SetActive(true);
+
+        //joins a chat channel - chat rooms dont need created, they just need a room name to work
+        channel = await socket.JoinChatAsync(RoomName, ChannelType.Room);
+        Debug.LogFormat("Join chat channel: {0} ", channel);
+
+    }
+
+    async void SendMessage()
+    {
+
+        string message = MessageInput.text;
+
+        //converts the message to a string
+        var content = new Dictionary<string, string> { { "Hello", message } }.ToJson();
+      
+        //when sending chat messages to a socket, it returns the state of the socket
+        //using _ basically discards the returned variable
+        _ = socket.WriteChatMessageAsync(channel, content);
+
+        MessageInput.text = "";
+    }
+
+    //closes the connection when the game quits
+    private void OnApplicationQuit()
+    {
+        //if socket is not null, close connection
+        socket?.CloseAsync();
+    }
 }
