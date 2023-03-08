2a0745ae96792a49c3d0747e97679ad07af9150c
FinlayMac <34044928+FinlayMac@users.noreply.github.com>
2023-03-08 01:57:51 +0000

    Added a quit game / add one to match win

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using Nakama;
+using Nakama.TinyJson;
+using UnityEngine.SceneManagement;
 public class StartGame : MonoBehaviour
 {
     private NakamaJoinMatch match;
+    private NakamaConnection connection;
+    private NakamaJoinMatch join;
+
+    public Button WinGameButton;
+
+    public UnityMainThreadDispatcher mainThread;
+
     private void Start()
     {
+        mainThread = UnityMainThreadDispatcher.Instance();
         match = FindObjectOfType<NakamaJoinMatch>();
 
         if (match == null)
@@ -14,7 +25,38 @@ public class StartGame : MonoBehaviour
             Debug.LogError("could not find Nakama Connection");
             return;
         }
+        connection = match.gameObject.GetComponent<NakamaConnection>();
+        join = match.gameObject.GetComponent<NakamaJoinMatch>();
+        WinGameButton.onClick.AddListener(ForceWinGame);
 
         match.StartGame();
+
+    }
+
+    private async void ForceWinGame()
+    {
+
+        var matchId = join.GetMatchID();
+        await connection.GetSocket().LeaveMatchAsync(matchId);
+
+
+        // for RPCs that require payloads
+        Dictionary<string, object> requestPayload = new Dictionary<string, object>()
+           {
+               { "prop1" , "value1" },
+               { "prop2" , "value2" }
+           };
+        var payload = Nakama.TinyJson.JsonWriter.ToJson(requestPayload);
+
+        //calls the custom rpc wingamestats
+        string winGameRpcId = "wingamestats";
+        IApiRpc responseData = await connection.GetSocket().RpcAsync(winGameRpcId, payload);
+        //connection.GetSession(),
+
+        mainThread.Enqueue(() =>
+               {
+                   SceneManager.LoadScene(0);
+                   //navigate to main menu
+               });
     }
 }
